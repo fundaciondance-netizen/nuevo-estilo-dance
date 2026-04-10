faff29f17830f51de1c56b577c53cc8f5695e302
dilansamugutierrez <dilansamugutierrez@gmail.com>
2026-04-10 11:32:38 -0500

    Cambios dispositivos moviles

--- components/back.tsx
+++ components/back.tsx
@@ -1,12 +1,14 @@
-"use client"
+"use client";
 
 export function BackgroundBreakSection() {
   return (
-    <section className="relative w-full h-[60vh]">
+    <section className="relative w-full h-[40vh] sm:h-[50vh] md:h-[60vh] overflow-hidden">
       <div
-        className="absolute inset-0 bg-cover bg-center bg-fixed"
+        className="absolute inset-0 bg-cover bg-no-repeat bg-center md:bg-fixed sm:bg-center"
         style={{ backgroundImage: "url('/images/hero_m.webp')" }}
       />
+
+      <div className="absolute inset-0 bg-black/15" />
     </section>
-  )
+  );
 }
